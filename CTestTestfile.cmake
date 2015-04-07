9d5c0436416984c65a88c0236a75cf8106f4bcdd
Joe Jackson <cpmhjoe@gmail.com>
2015-04-07 13:29:55 -0400

    Remove cmake generated files

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joe/src/wu-lab-recolor
-# Build directory: /home/joe/src/wu-lab-recolor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LabRecolorTest "/home/joe/src/wu-lab-recolor/test/Test")
-subdirs(src)
-subdirs(test)
